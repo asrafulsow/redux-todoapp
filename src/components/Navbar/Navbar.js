fa725e4caa334454707cd2b83e7bc2c83217651c
asrafulsow <aisowrav11@gmail.com>
2023-04-07 23:25:37 +0600

    e

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,7 +4,6 @@ const Navbar = () => {
     return (
         <div className="fixed top-0 left-0 text-center w-full header bg-violet-600 py-4 text-white font-bold text-lg shadow-lg">
             Simple Todo Application with Redux
-            <span>  G</span>
         </div>
     );
 };
